692ba2fabdbb45bc283fb3e92df225b915bc02c0
Not-Darshil <darshilkumar0353@gmail.com>
2023-07-15 00:07:59 +0530

    Push DONE

--- bettercalculator copy.py
+++ bettercalculator copy.py
@@ -4,69 +4,55 @@ win = Tk(className='Calculator By Darshil')
 win.geometry("415x550")
 win.configure(bg="black")
 out_put = ""
-lst = [""]
-k = []
+val = ""
 
 
 def button(x1, y1, x2, y2, txt):
-    button = Button(win, text=str(txt), height=(y2-y1),width=(x2-x1), command=lambda: fx(txt))
+    button = Button(win, text=str(txt), height=(y2-y1),
+                    width=(x2-x1), command=lambda: fx(txt))
+    button["font"] = ("Arial", 9)
     button.place(x=x1, y=y1)
     return
 
 
 def change_output(x=out_put):
     lab.config(text=x)
-    
-    
+    return
 
 
 def button_press(a):
     global out_put
-    global lst
-    global k
-    print("lst:",lst)
-    if a in "1234567890+-*/":
-        if a in "1234567890":
-            out_put = out_put + a
-            change_output(out_put)
-        elif a in "+-*/":
-            lst.append(out_put)
-            change_output(a)
-            out_put =" "
-    if a in "+-*/":
-        c=0
-        for i in range(-1,-len(lst)-1,-1):
-            if lst[i] in range(9**999) :#list check krni pdegi
-                c=0
-            if lst[i] in "+-*/":
-                if c==0:
-                    lst.append(a)   
-                    c=1
-                
-    
-
+    global val
+    if a in "1234567890":
+        out_put = out_put + a
+        val = val+a
+        change_output(out_put)
     elif a == "=":
-        st = ""
-        for k in lst:
-            st = st+str(k)
-        print(st)
-        lst = []
-        lst.append(str(eval(st)))
-        out_put = str(eval(st))
+        val = "(" + val + ")"
+        out_put = str(eval(val))
         change_output(out_put)
-        out_put = ""
-        
+    elif a in "+-/*":
+        if val[-1] not in "+-/*":
+            val = val+a
+            out_put = a
+            change_output(out_put)
+
     elif a == "Delete":
-        out_put = out_put[:-1:1]
+        if val[-1] != ")":
+            val = val[:-1:1]
+            out_put = out_put[:-1:1]
         change_output(out_put)
-    elif a == "All\nClear":
+
+    elif a == "AC":
         out_put = ""
-        lst = []
+        val = ""
         change_output(out_put)
+
 
 def fx(x):
     change_output()
     button_press(x)
+
 
 # input and output display
 lab = Label(win, text="", height=7, width=53, bg="white")
@@ -88,9 +74,8 @@ button(220, 345, 230, 350, "9")
 button(320, 345, 330, 350, "/")
 # 4th line
 button(20, 445, 25, 450, "Delete")
-button(75, 445, 79, 450, "All\nClear")
+button(75, 445, 79, 450, "AC")
 button(120, 445, 130, 450, "0")
 button(220, 445, 230, 450, "=")
 button(320, 445, 330, 450, "*")
 win.mainloop()
-
